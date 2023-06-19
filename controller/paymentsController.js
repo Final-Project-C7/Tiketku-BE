e6a9013cba13d46b957c748f5b81828589e3eece
Danarzlf <Danarzlf10@example.com>
2023-06-19 19:48:13 +0700

    feat/api-payments

--- controller/paymentsController.js
+++ controller/paymentsController.js
@@ -1,6 +1,9 @@
 const { payments } = require("../models");
 const catchAsync = require("../utils/catchAsync");
+const ApiError = require("../utils/ApiError");
+const httpStatus = require("http-status");
 
+//CREATE
 const createPayments = catchAsync(async (req, res) => {
   const { booking_id, payment_method, payment_amount, payment_date } = req.body;
 
@@ -19,6 +22,92 @@ const createPayments = catchAsync(async (req, res) => {
   });
 });
 
+// UPDATE
+const updatePayment = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const { booking_id, payment_method, payment_amount, payment_date } = req.body;
+
+  const payment = await payments.findByPk(id);
+
+  if (!payment) {
+    throw new ApiError(404, `Pembayaran dengan id ${id} tidak ditemukan`);
+  }
+
+  payment.booking_id = booking_id;
+  payment.payment_method = payment_method;
+  payment.payment_amount = payment_amount;
+  payment.payment_date = payment_date;
+
+  await payment.save();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      payment,
+    },
+  });
+});
+
+//GET ALL
+const findAllPayments = catchAsync(async (req, res) => {
+  const paymentsData = await payments.findAll();
+  res.status(200).json({
+    status: "success",
+    data: {
+      payments: paymentsData,
+    },
+  });
+});
+
+//GET BY ID
+const findPaymentsById = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const payment = await payments.findByPk(id);
+
+  if (!payment) {
+    throw new ApiError(
+      httpStatus.NOT_FOUND,
+      `Pembayaran dengan id ${id} tidak ditemukan`
+    );
+  }
+
+  res.status(200).json({
+    status: "Success",
+    data: {
+      payment,
+    },
+  });
+});
+
+//DELETE
+const deletePayments = catchAsync(async (req, res) => {
+  const id = req.params.id;
+
+  const payment = await payments.findByPk(id);
+
+  if (!payment) {
+    throw new ApiError(
+      httpStatus.NOT_FOUND,
+      `Pembayaran dengan id ${id} tidak ditemukan`
+    );
+  }
+
+  await payments.destroy({
+    where: {
+      id,
+    },
+  });
+
+  res.status(200).json({
+    status: "Success",
+    message: `Pembayaran dengan id ${id} telah dihapus`,
+  });
+});
+
 module.exports = {
   createPayments,
+  updatePayment,
+  findAllPayments,
+  findPaymentsById,
+  deletePayments,
 };
--- routes/payments.js
+++ routes/payments.js
@@ -8,5 +8,9 @@ const Auth = require("../middlewares/authenticate");
 
 // API
 router.post("/", Auth, paymentsController.createPayments);
+router.put("/:id", Auth, paymentsController.updatePayment);
+router.get("/", Auth, paymentsController.findAllPayments);
+router.get("/:id", Auth, paymentsController.findPaymentsById);
+router.delete("/:id", Auth, paymentsController.deletePayments);
 
 module.exports = router;
