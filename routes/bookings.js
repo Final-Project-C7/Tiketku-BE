f1ee3b72619d9032adcde5e411f37557a02d25a3
adikrnwn171 <adikrnwn171@gmail.com>
2023-06-19 20:30:26 +0700

    feat/update API flight and API booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -9,5 +9,6 @@ const Auth = require("../middlewares/authenticate");
 // API
 router.post("/", Auth, bookingsController.createBookings);
 router.get("/:id", Auth, bookingsController.getBookingsById);
+router.put("/:id", Auth, bookingsController.updateBooking);
 
 module.exports = router;
