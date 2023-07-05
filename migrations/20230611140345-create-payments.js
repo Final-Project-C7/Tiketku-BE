1cb40fa7f49bca00fe56db890c3907304abf1e81
FerdyLazuardi <ferdy.lazuardi2000@gmail.com>
2023-07-06 03:12:58 +0700

    model update

--- migrations/20230611140345-create-payments.js
+++ migrations/20230611140345-create-payments.js
@@ -1,37 +1,40 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('payments', {
+    await queryInterface.createTable("payments", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       booking_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       payment_method: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       payment_amount: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       payment_date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+      },
+      payment_status: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('payments');
-  }
+    await queryInterface.dropTable("payments");
+  },
 };
